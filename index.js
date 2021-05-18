79717b4c22bb3112c7181da56a07b940f3de5e51
Vivek <vivek0094.cse19@chitkara.edu.in>
2021-05-18 22:33:42 +0530

    fixing lines

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sgMail = require('@sendgrid/mail')
 const app = express();
 
 const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=110037&date=20-05-2021";
-const sendgridAPIKey = process.env.SENDGRID_API_KEY;
+const sendgridAPIKey = process.env.SENDGRID_API_KEY || "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";
 const port = process.env.PORT || 3000;
 sgMail.setApiKey(sendgridAPIKey)
 
@@ -14,7 +14,7 @@ setInterval(() => {
         if(error) {
             return console.log("i got some error")
         }
-        if(body.sessions.length && body.sessions[0].min_age_limit == 18) {
+        if(body.sessions.length > 0 && body.sessions[0].min_age_limit == 18) {
             sgMail.send({
                 to: 'vivek.gugnani04@gmail.com',
                 from: 'vivek0094.cse19@chitkara.edu.in',
