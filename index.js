bc865fa826d5c1e56c095d18ebc597860722c0ee
Vivek <vivek0094.cse19@chitkara.edu.in>
2021-05-18 22:24:30 +0530

    testing app

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const request = require('request');
 const sgMail = require('@sendgrid/mail')
 const app = express();
 
-const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=136135&date=20-05-2021";
+const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=110037&date=20-05-2021";
 const sendgridAPIKey = process.env.SENDGRID_API_KEY;
+const port = process.env.PORT || 3000;
 sgMail.setApiKey(sendgridAPIKey)
+
+app.use(express.json)
 setInterval(() => {
     request({url , json:true}, (error, {body}) => {
         if(error) {
@@ -27,4 +30,10 @@ setInterval(() => {
     })
 },5000)
 
-app.listen(process.env.PORT)
+app.get('/', (req, res) => {
+    res.render('<h4><b>Everything is fine here!</b></h4>')
+})
+
+app.listen(port, () => {
+    console.log('server is up on port '+port)
+})
