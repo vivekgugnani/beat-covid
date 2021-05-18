0ec552f8ed2cab1ddb1bd3d78c09f62ed4cd2eea
Vivek <vivek0094.cse19@chitkara.edu.in>
2021-05-18 22:45:49 +0530

    checking heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sgMail = require('@sendgrid/mail')
 const app = express();
 
 const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=110037&date=20-05-2021";
-const sendgridAPIKey = process.env.SENDGRID_API_KEY || "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";
+const sendgridAPIKey = process.env.SENDGRID_API_KEY;
 const port = process.env.PORT || 3000;
 sgMail.setApiKey(sendgridAPIKey)
 
@@ -14,27 +14,30 @@ setInterval(() => {
         if(error) {
             return console.log("i got some error")
         }
-        if(!body.sessions) {
-            return console.log('nothing found')
-        }
-        if(body.sessions.length > 0 && body.sessions[0].min_age_limit == 18) {
-            sgMail.send({
-                to: 'vivek.gugnani04@gmail.com',
-                from: 'vivek0094.cse19@chitkara.edu.in',
-                subject: 'AA gya injection',
-                text:   'jldi se book  kro'
-            }).then(() => {
-                console.log('sent')
-            }).catch((e) => {
-                console.log(e)
-            })
-            console.log(body.sessions)
-        }
+        // if(!body.sessions) {
+        //     return console.log('nothing found')
+        // }
+        console.log(body)
+        // if(body.sessions.length > 0 && body.sessions[0].min_age_limit == 18) {
+        //     sgMail.send({
+        //         to: 'vivek.gugnani04@gmail.com',
+        //         from: 'vivek0094.cse19@chitkara.edu.in',
+        //         subject: 'AA gya injection',
+        //         text:   'jldi se book  kro'
+        //     }).then(() => {
+        //         console.log('sent')
+        //     }).catch((e) => {
+        //         console.log(e)
+        //     })
+        //     console.log(body.sessions)
+        // }
     })
 },5000)
 
 app.get('/', (req, res) => {
-    res.render('<h4><b>Everything is fine here!</b></h4>')
+    res.render({
+        hi: "everything fine!"
+    })
 })
 
 app.listen(port, () => {
